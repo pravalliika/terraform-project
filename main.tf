e53dbd3f04394c2666f8f2636390fc4e1a57a9c1
pravallika96973 <pravallika96973@gmail.com>
2024-10-06 16:45:59 +0530

    Added s3 bucket to Terraform-project

--- main.tf
+++ main.tf
@@ -6,5 +6,21 @@ provider "aws" {
 resource "aws_instance" "ec2-user" {
  ami           = "ami-00f251754ac5da7f0" #Amazon Linux 2 AMI
  instance_type = "t2.micro"
-  
+
+ tags = {
+    Name = "MYfirstEc2instance"
+ }
+
 }
+ resource "aws_s3_bucket" "my_gar"  {
+  bucket  = "my-bucket-for-terraform-18923"  #unique name globally
+  
+  tags =  {
+    Name       = "my_gar"
+    Environment = "Dev"
+
+  
+  }
+
+ }
+  
